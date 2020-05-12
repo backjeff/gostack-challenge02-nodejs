662253b470271ea4eba43b9021db1110f8ca3886
Jefferson Silva <jefferson.silva-ext@ab-inbev.com>
2020-05-12 00:23:57 -0300

    :rocket: First version of challenge code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,73 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepository(request, response, next) {
+  const { id } = request.params;
+  
+  if(!isUuid(id)) {
+    return response.status(400).json({
+      message: 'Invalid repository id'
+    });
+  }
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  
+  if(repositoryIndex < 0) {
+    return response.status(400).json({
+      message: 'Repository not found'
+    });
+  }
+
+  request.repositoryIndex = repositoryIndex;
+  request.repository = repositories[repositoryIndex];
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  var repository = { 
+    id: uuid(), title, url, techs, likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateRepository, (request, response) => {
+  const { title, url, techs } = request.body;
+  
+  var repository = {
+    ...request.repository,
+    title, url, techs
+  }
+
+  repositories[request.repositoryIndex] = repository
+
+  return response.status(200).json(repository);
+
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateRepository, (request, response) => {
+  repositories.splice(request.repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateRepository, (request, response) => {
+  var repository = {
+    ...request.repository,
+    likes: ++request.repository.likes
+  }
+
+  repositories[request.repositoryIndex] = repository
+
+  return response.status(200).json(repository);
 });
 
 module.exports = app;
